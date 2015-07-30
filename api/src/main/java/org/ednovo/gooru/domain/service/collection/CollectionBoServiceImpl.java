bc38db4186937eb9d3ce25666824028adfb270e0
Sheeban Singaram <sheeban.s@goorulearning.org>
2015-07-30 15:46:56 +0530

    added fix for updating the resource count.

--- api/src/main/java/org/ednovo/gooru/domain/service/collection/CollectionBoServiceImpl.java
+++ api/src/main/java/org/ednovo/gooru/domain/service/collection/CollectionBoServiceImpl.java
@@ -640,7 +640,7 @@ public class CollectionBoServiceImpl extends AbstractResourceServiceImpl impleme
 		} else {
 			getCollectionDao().remove(collectionItem);
 		}
-		updateCollectionMetaDataSummary(collectionContentId, RESOURCE, contentType);
+		updateCollectionMetaDataSummary(collectionContentId, contentType, DELETE);
 	}
 
 	private Errors validateResource(final Resource resource) {
